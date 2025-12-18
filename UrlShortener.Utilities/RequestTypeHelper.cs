10ce3b5c9640569fcd952a61e8507d997ec313b0
Chuma <chuxnwuba@gmail.com>
2025-12-17 22:20:55 -0500

    Refactored conditionals

--- UrlShortener.Utilities/RequestTypeHelper.cs
+++ UrlShortener.Utilities/RequestTypeHelper.cs
@@ -8,24 +8,18 @@ namespace Utilities
         {
             if (request == null)
                 throw new ArgumentNullException(nameof(request), "HTTP request cannot be null");
-
-            if (request.Headers.TryGetValue("User-Agent", out var userAgent))
-            {
-                if (userAgent.ToString().ToLower().Contains("postman"))
-                    return false;
-            }
-
-            if (request.Headers.TryGetValue("Sec-Fetch-Mode", out var mode))
-            {
-                if (mode.ToString().ToLower() == "cors")
-                    return false;
-            }
-
-            if (request.Headers.TryGetValue("Referer", out var referer))
-            {
-                if (referer.ToString().ToLower().Contains("swagger"))
-                    return false;
-            }
+            
+            if (request.Headers.TryGetValue("User-Agent", out var userAgent) &&
+                userAgent.ToString().ToLower().Contains("postman"))
+                return false;
+            
+            if (request.Headers.TryGetValue("Sec-Fetch-Mode", out var mode) &&
+                mode.ToString().ToLower() == "cors")
+                return false;
+            
+            if (request.Headers.TryGetValue("Referer", out var referer) &&
+                referer.ToString().ToLower().Contains("swagger"))
+                return false;
             return true;
         }
     }
